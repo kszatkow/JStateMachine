d1923a2cdeb943140edefe49e30706b0f7e29f1d
kszatkow <k.d.szatkowski@gmail.com>
2016-10-23 22:12:21 +0200

    Added TODO for junction state implementation.

--- src/org/moomin/statemachine/JunctionState.java
+++ src/org/moomin/statemachine/JunctionState.java
@@ -1,5 +1,6 @@
 package org.moomin.statemachine;
 
+// TODO implement real junction state logic here
 public class JunctionState extends NoBehaviourSimpleState {
 
 	public JunctionState(String string) {	}
