5580f6fa873d49f789d7c521921fc46944a8afef
kszatkow <k.d.szatkowski@gmail.com>
2016-10-31 20:24:22 +0100

    Removed invalid TODO.

--- test/org/moomin/statemachine/phone/PartialDial.java
+++ test/org/moomin/statemachine/phone/PartialDial.java
@@ -1,24 +1,9 @@
 package org.moomin.statemachine.phone;
 
-import org.moomin.statemachine.SimpleState;
+import org.moomin.statemachine.NoBehaviourSimpleState;
 
-public class PartialDial extends SimpleState {
+public class PartialDial extends NoBehaviourSimpleState {
 
 	public PartialDial(String string) {}
-
-	@Override
-	public void onEntryBehaviour() {
-		// TODO add number handling implementation
-	}
-
-	@Override
-	public void doActionBehaviour() {
-		// empty on purpose
-	}
-
-	@Override
-	public void onExitBehaviour() {
-		// empty on purpose
-	}
 
 }
